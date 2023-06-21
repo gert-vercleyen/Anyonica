3d348057b8649878258b85b622cb5f8941d16fb9
gert-vercleyen <86326111+gert-vercleyen@users.noreply.github.com>
2023-06-21 14:23:12 +0200

    Increased speed

--- ReductionTools/ReduceByBinomials.m
+++ ReductionTools/ReduceByBinomials.m
@@ -18,7 +18,7 @@ ReduceByBinomials::notlistofvars =
   "`1` is not a list of variables.";
 
 Options[ ReduceByBinomials ] :=
-  Options[ SolvePolynomialSystem ];
+  Options[ SolveBinomialSystem ];
 
 CheckArgs[ sumEqns_, binomialEqns_, vars_ ] :=
   Which[
@@ -42,7 +42,7 @@ ReduceByBinomials[ sumEqns_, binomialEqns_, vars_, s_, opts:OptionsPattern[] ] :
 (
   CheckArgs[ sumEqns, binomialEqns, vars ];
   Module[{
-    SolveRepeatedly, firstSoln, constraints, invertibleMatrices, polynomialConstraints, simplify,
+    SolveRepeatedly, firstSystems, constraints, invertibleMatrices, polynomialConstraints, simplify,
     preEqCheck, procID, absTime, result
     },
     invertibleMatrices =
@@ -69,62 +69,54 @@ ReduceByBinomials[ sumEqns_, binomialEqns_, vars_, s_, opts:OptionsPattern[] ] :
       SolveRepeatedly[ { {}, nonBinEqns_ }, _, _, prevSols_, _  ] :=
         Sow[ { nonBinEqns, prevSols } ];
 
-      SolveRepeatedly[ { binEqns_, nonBinEqns_ }, variables_, s[i_], prevSols_, { pConstr_, invMats_ } ] :=
-      Module[ { validSoln },
+      SolveRepeatedly[ { binEqns_, nonBinEqns_ }, variables_, s[i_], prevSols_, constr_ ] :=
+        Module[ { updatedSystems },
+          updatedSystems =
+            AddOptions[opts][SolveAndUpdate][
+              binEqns, nonBinEqns, constr, variables, s[i],
+              "NonSingular"   -> True,
+              "Symmetries"    -> None
+              (* Symmetries are exhausted by first call. If not set to None, the original symmetries are used and an error occurs  *)
+            ];
 
-        If[ pConstr === { False } || invMats === { False }, Return @ {} ];
+          If[ updatedSystems === {}, Return @ {} ];
 
-        validSoln =
-          SolveAndCheck[
-            binEqns,
-            variables,
-            s[i],
-            "PolynomialConstraints" -> pConstr,
-            "InvertibleMatrices" -> invMats,
-            "NonSingular" -> True,
-            "SumEquations" -> nonBinEqns,
-            "PreEqualCheck" -> preEqCheck
-          ];
+          Do[
+            SolveRepeatedly[
+              BinSplit[ sys["Equations"], BinomialEquationQ @* preEqCheck ],
+              GetVariables[ Normal @ sys["Equations"], s[i] ] ,
+              s[i+1],
+              Normal @ prevSols /. sys["Solution"],
+              sys["Constraints"]
+            ]
+            ,
+            { sys, updatedSystems }
+          ]
+        ];
 
-        If[ validSoln === {}, Return @ {} ];
+      firstSystems =
+        AddOptions[opts][SolveAndUpdate][
+          binomialEqns,
+          sumEqns,
+          { polynomialConstraints, invertibleMatrices },
+          vars,
+          s[1]
+        ];
 
-        MapThread[
-          SolveRepeatedly[
-            UpdateSystem[ nonBinEqns, #1, simplify, preEqCheck ],
-            GetVariables[ Normal[#1], s[i] ] ,
-            s[i + 1],
-            Normal[prevSols] /. #1,
-            #2
-          ]&,
-          {
-            Dispatch /@ validSoln,
-            UpdateConstraints[ { pConstr, invMats }, #, simplify, preEqCheck ]& /@
-            validSoln
-          }
-        ]
-      ];
-
-
-      firstSoln =
-        AddOptions[opts][SolveAndCheck][ binomialEqns, vars, s[1] ];
-
-      If[ firstSoln === {}, Return @ {} ];
+      If[ firstSystems === {}, Return @ {} ];
 
       Reap[
-        MapThread[
+        Do[
           SolveRepeatedly[
-            UpdateSystem[ sumEqns, #1, simplify, preEqCheck ],
-            GetVariables[ Normal @ #1, s[1] ],
+            BinSplit[ sys["Equations"], BinomialEquationQ @* preEqCheck ],
+            GetVariables[ Normal @ sys["Solution"], s[1] ],
             s[2],
-            Normal @ #1,
-            #2
-          ]&,
-          {
-            Dispatch /@ firstSoln,
-            UpdateConstraints[ { polynomialConstraints, invertibleMatrices }, #, simplify, preEqCheck ]& /@
-            firstSoln
-          }
-        ]
+            Normal @ sys["Solution"],
+            sys["Constraints"]
+          ]
+          ,
+          { sys, firstSystems }
+        ];
       ][[2]] /. ( s[_][i_] :> s[i] ) /. ( {x_List} :> x )
     ];
 
@@ -144,113 +136,120 @@ RBB =
   ReduceByBinomials;
 
 
-Options[SolveAndCheck] :=
-  Join[ 
-    Options[ReduceByBinomials], 
-    { "SumEquations" -> {} } 
-  ];
+Options[SolveAndUpdate] :=
+  Options[ReduceByBinomials];
 
-SolveAndCheck[ binEqns_, vars_, s_, opts:OptionsPattern[] ] :=
-  Module[{ soln, simplify, sumEqns, preEqCheck, procID, absTime, result },
+(* Solves binEqns, updates sumEqns and constraints and checks for validity.
+  A list of triples { sumEqns_i, sol_i, constr_i } is returned
+*)
+SolveAndUpdate[ binEqns_, sumEqns_, constraints_, vars_, s_, opts:OptionsPattern[] ] :=
+  Module[{ soln, solve, preEqCheck, simplify, simplifySolutions, procID, absTime, result, eqnSolConstr, notInvalidPos },
     simplify =
+      OptionValue["SimplifyIntermediateResultsBy"];
+    simplifySolutions =
       Function[
         solutions,
         If[
           solutions =!= {},
-          MapAt[
-            OptionValue["SimplifyIntermediateResultsBy"],
-            solutions,
-            { All, All, 2 }
-          ],
+          MapAt[ simplify, solutions, { All, All, 2 } ],
           {}
         ]
       ];
-    sumEqns =
-      OptionValue["SumEquations"];
     preEqCheck =
       OptionValue["PreEqualCheck"];
+    solve =
+      If[ OptionValue["NonSingular"], SNSBS, SBS ];
     procID =
       ToString[ Unique[] ];
     
-    printlog[ "SAC:init", {procID, binEqns,vars,s,{opts}} ];
+    printlog[ "SAU:init", {procID, binEqns,vars,s,{opts}} ];
 
     { absTime, result } =
       AbsoluteTiming[
-        Select[
-          soln =
-            simplify @
-            AddOptions[opts][
-              If[
-                OptionValue["NonSingular"],
-                SolveNonSingularBinomialSystem,
-                SolveBinomialSystem
-              ]
-              ][ binEqns, vars, s ],
-          NotInvalidNonZeroSolutionQ[ sumEqns, preEqCheck ]
+        (* Note that we simplify solutions first and then simplify sumEqns again. This is to reduce memory pressure *)
+        soln =
+          Map[
+            Dispatch,
+            simplifySolutions @ AddOptions[opts][solve][ binEqns, vars, s ]
+          ];
+
+        eqnSolConstr =
+          Table[
+            {
+              TEL @ AddOptions[opts][UpdateAndCheck][ sumEqns, sol, Identity ],
+              sol,
+              AddOptions[opts][UpdateConstraints][ constraints, sol ]
+            },
+            { sol, soln }
+          ];
+
+        printlog["SAU:updated_sys", {procID, eqnSolConstr[[;;,1]], eqnSolConstr[[;;,3]] } ];
+
+        notInvalidPos =
+          Flatten @
+          Position[
+            eqnSolConstr,
+            { e_, sl_, c_ } /;
+            NotInvalidNonZeroSolutionQ[e,preEqCheck][sl] && c =!= {False},
+            {1}
+          ];
+
+        If[
+          Length[notInvalidPos] != Length[soln],
+          printlog["SAU:invalid_positions", {procID, Complement[ Range @ Length @ soln, notInvalidPos ] } ]
+        ];
+
+        Table[
+          <|
+            "Equations"   -> eqnSolConstr[[i,1]],
+            "Solution"    -> eqnSolConstr[[i,2]],
+            "Constraints" -> eqnSolConstr[[i,3]]
+          |>,
+          { i, notInvalidPos }
         ]
+
       ];
     
-    printlog["SAC:remainingsol", {procID,soln,result}];
+    printlog["SAU:remainingsol", {procID,soln,result}];
     printlog["Gen:results", {procID,result,absTime}];
   
     result
   ];
 
-UpdateSystem[ eqns_, solution_, simplify_, preEqCheck_ ] :=
-  With[{
-    updatedSystem =
-      Map[
-        simplify,
-        eqns/.solution,
-        {2}
-      ] //
-      Select[ Not[ TrueQ[ preEqCheck[ # ] ] ]& ] //
-      DeleteDuplicates
-    },
-    BinSplit[ updatedSystem, BinomialEquationQ @* preEqCheck ]
-  ];
 
-UpdateConstraints[ { polConstr_, invertibleMatrices_ }, solution_, simplify_, preEqCheck_ ] :=
-  Module[ { newPolConstr, newInvMats },
+Options[UpdateConstraints] =
+  {
+    "SimplifyIntermediateResultsBy" -> Identity,
+    "PreEqualCheck" -> Identity
+  };
+
+UpdateConstraints[ { polConstr_, invertibleMatrices_ }, solution_, opts:OptionsPattern[] ] :=
+  Module[ { newPolConstr, newInvMats, simplify, preEqCheck },
+    simplify =
+      OptionValue["SimplifyIntermediateResultsBy"];
+    preEqCheck =
+      OptionValue["PreEqualCheck"];
+
     newPolConstr =
-      Select[
-        Map[
-          simplify,
-          polConstr/.solution,
-          {2}
-        ],
-        Not[TrueQ[preEqCheck[#]]]&
-      ];
+      AddOptions[opts][UpdateAndCheck][ polConstr, solution, Identity ];
 
-    If[
-      MemberQ[False] @ newPolConstr,
-      Return[ { { False }, {} } ]
-    ];
+    If[ newPolConstr === { False }, Return @ { False } ];
 
     newInvMats =
-      DeleteDuplicatesBy[
-        Map[
-          simplify,
-          invertibleMatrices/.solution,
-          {2}
-        ] // DeleteCases[ mat_/; TrueQ[ Det[mat] != 0 ] ],
-        Abs @* Det
-      ];
-    
-    If[
-      MemberQ[ newInvMats, mat_/; simplify[Det[mat]] == 0 ],
-      Return[ { {}, { False } } ]
-    ];
+      AddOptions[opts][UpdateAndCheck][ invertibleMatrices, solution, Det[#] =!= 0& ];
 
-    { newPolConstr, newInvMats }
+    If[ newInvMats === { False }, Return @ { False } ];
+
+    {
+      Select[ Not @* TrueQ @* preEqCheck ] @ newPolConstr,
+      DeleteCases[ mat_/; TrueQ[ Det[mat] != 0 ] ] @ newInvMats
+    }
+
   ];
 
 UpdateInvertibleMatrices[ {}, _ ] :=
   {};
 
 UpdateInvertibleMatrices[ mats_List, soln_ ] :=
-  DeleteDuplicates[
     ( mats /. Dispatch[soln] ) //
-    DeleteCases[ mat_/; TrueQ[Det[mat] != 0] ],
-    Abs[Det[#1]] === Abs[Det[#2]]&
-  ];
+    DeleteCases[ mat_/; TrueQ[Det[mat] != 0] ];
