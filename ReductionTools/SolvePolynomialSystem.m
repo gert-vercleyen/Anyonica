43bb194f0a58648fc6ac01f4df56f0d3d3e500ae
gert-vercleyen <gert.vercleyen@protonmail.com>
2023-10-19 14:50:11 +0200

    Update SolvePolynomialSystem.m
    + new attempt for SolvePolynomialSystem.m

--- ReductionTools/SolvePolynomialSystem.m
+++ ReductionTools/SolvePolynomialSystem.m
@@ -6,12 +6,16 @@ PackageExport["SolvePolynomialSystem"]
 
 SolvePolynomialSystem::usage =
   "SolvePolynomialSystem[ eqns, vars, s ] returns the solutions, parametrized by s, "<>
-  "to the system of polynomial equations eqns in the variables vars.";
+  "to the system of polynomial equations eqns in the variables vars.\n" <>
+  "SolvePolynomialSystem[ polynomials, vars, s ] returns the solutions, parametrized by s, "<>
+  "to the system of polynomials in the variables vars.\n"<>
+  "SolvePolynomialSystem[ sys, s ] returns the solutions, parametrized by s, "<>
+  "to the Standard Polynomial System sys.";
 
 Options[ SolvePolynomialSystem ] :=
   Options[SolveBinomialSystem];
 
-SolvePolynomialSystem[ eqns_List, vars_List, symbol_, opts:OptionsPattern[] ] :=
+(*SolvePolynomialSystem[ eqns_List, vars_List, symbol_, opts:OptionsPattern[] ] :=
   Module[{
     newEquations, newSymmetries, newPolConstraints, newInvertibleMats, newVars, newZeroVals, revertVars,
     binomialEquations, sumEquations, sumEqnSolnPairs, sumEqns, monSol, sumSolution, lastVars, solutions,
@@ -114,3 +118,165 @@ SolvePolynomialSystem[ eqns_List, vars_List, symbol_, opts:OptionsPattern[] ] :=
 
     result
   ];
+*)
+
+SolvePolynomialSystem[ sys_SPS, s_, opts:OptionsPattern[] ] :=
+  Module[{ OV, pols, assum, rules, vars, zeroConfigs, bins, nonbins, procID, validPos, systems, symmetries },
+    OV = OptionValue;
+    
+    procID = ToString @ Unique[];
+    
+    printlog["SPS:init",{ procID, sys, s, {opts} }];
+    
+    { pols, assum, rules, vars } = GetData[ sys ];
+    
+    { time, results } =
+    AbsoluteTiming[
+      
+      { bins, nonbins } =
+        BinomialSplit @ pols;
+      
+      printlog[ "SPS:zero_FS", { procID } ];
+      
+      (* ================================================= *)
+      (* Find which configurations of variables can be 0 = *)
+      (* ================================================= *)
+      
+      zeroConfigs =
+        Dispatch /@
+        Which[
+          OV["NonSingular"],       {{}},
+          OV["ZeroValues"] =!= {}, OV["ZeroValues"],
+          True,                    AddOptions[opts][FindZeroValues][ If[ OV["FindZerosUsingSums"], pols, bins ] ]
+        ];
+      
+      (* Check the configurations with the rest of the system *)
+      validPos =
+        Position[ zeroConfigs, z_ /; ValidZerosQ[pols] @ z, {1} ] // Flatten;
+      
+      printlog[" SPS:zero_values_results", { procID, zeroConfigs, Complement[ Range @ Length @ zeroConfigs, validPos ] } ];
+      
+      (* Remove invalid configurations *)
+      zeroConfigs = zeroConfigs[[ validPos ]];
+      
+      If[ (* No valid configurations *)
+        Length[zeroConfigs] == 0,
+        printlog["SPS:no_valid_zeros"]; Return @  { }
+      ];
+      
+      (* Create systems for each configuration of zero values *)
+      systems = UpdateRules[ sys, # ]& /@ zeroConfigs;
+      
+      (* ================================================= *)
+      (*               Break gauge symmetry                *)
+      (* ================================================= *)
+      
+      symmetries = OV["Symmetries"];
+      
+      (* Check whether symmetries are multiplicative *)
+      If[
+        symmetries =!= None && symmetries["Transforms"][[;;, 1]] =!= vars && symmetries["Transforms"] =!= {},
+        Message[ SolvePolynomialSystem::wrongsymmetriesformat, symmetries["Transforms"][[;;, 1]], vars ];
+        Abort[]
+      ];
+     
+      If[
+        symmetries =!= None,
+        systems = AddOptions[opts][BreakAllMultiplicativeSymmetry][ symmetries, vars, systems ]
+      ];
+      
+      (* ================================================= *)
+      (*             Reduce via trivialities               *)
+      (* ================================================= *)
+      
+      (* ================================================= *)
+      (*               Reduce via Binomials                *)
+      (* ================================================= *)
+      
+      (* ================================================= *)
+      (*              Reduce via Linear Terms              *)
+      (* ================================================= *)
+      
+      (* ================================================= *)
+      (*          Solve equations with 1 variable          *)
+      (* ================================================= *)
+      
+      (* ================================================= *)
+      (*                Find Groebner Bases                *)
+      (* ================================================= *)
+      
+      (* ================================================= *)
+      (*             Solve Systems of Equations            *)
+      (* ================================================= *)
+      
+    ];
+    
+    results
+  ];
+    
+
+(* BreakAllMultiplicativeSymmetry updates systems by fixing the gauge symmetry for all configurations of 0 values
+   by first breaking symmetry for all symbols that do not involve 0 values and then
+   breaking further symmetry, case by case.
+ *)
+
+Options[BreakAllMultiplicativeSymmetry] :=
+  Options[BreakMultiplicativeSymmetry];
+
+BreakAllMultiplicativeSymmetry[ symmetries_, variables_, systems_, opts:OptionsPattern[] ] :=
+  Module[ { zeroConfigs, unionZeros, sharedVars, firstFixedVars, remainingSym, fixedVars },
+    zeroConfigs =
+      Cases[ HoldPattern[ _ -> 0 ] ] @* GetRules /@ systems;
+    
+    If[
+      zeroConfigs == { { } }
+      ,
+      fixedVars = Last @ AddOptions[opts][BreakMultiplicativeSymmetry][symmetries];
+      Return @ { UpdateValues[ First @ systems, firstFixedVars ] }
+    ] ;
+    
+    (* Break Gauge Symmetry: first for all variables that are never 0, i.e.
+       that do not appear in any of the "zeros" from previous step *)
+    
+    (* Get all vars that could be 0 for some configuration in zeros *)
+    unionZeros = Union @@ Normal[zeroConfigs][[;;,;;,1]];
+    
+    (* Get all vars that can never be 0 *)
+    sharedVars = Complement[ variables, unionZeros ];
+    
+    (* Fix the gauge for all vars that can never be 0 *)
+    { remainingSym, firstFixedVars } =
+      AddOptions[opts][BreakMultiplicativeSymmetry][ symmetries, "ExcludedVariables" -> unionZeros ];
+    
+    
+    (* Try to fix extra gauges, if possible, for each of the 0-configurations. *)
+    
+    If[ (* All gauges are fixed *)
+      Equivalent @@@ remainingSym["Transforms"]
+      ,
+      (* THEN: no gauge freedom left. Return updated systems *)
+      Table[
+        UpdateValues[ sys, firstFixedVars ],
+        { sys, systems }
+      ]
+      ,
+      (* ELSE: still gauge freedom left. Breaking further symmetry per set of zero values *)
+      Table[
+        fixedVars =
+          Join[
+            firstFixedVars,
+            Last @ BreakMultiplicativeSymmetry[ AddZerosToSymmetries[ remainingSym, zeroConfigs[[i]] ] ]
+          ];
+        
+        UpdateValues[ systems[[i]], fixedVars ]
+        ,
+        { i, Length @ systems }
+      ]
+    ]
+  ];
+
+
+PackageScope["ValidZerosQ"]
+
+ValidZerosQ[ pols_ ][ zeros_ ] :=
+  FreeQ[ pols/.Dispatch[zeros], x_?MonomialQ /; x =!= 0 ];
