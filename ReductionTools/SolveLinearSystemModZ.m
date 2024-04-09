3812a84ea8db97d58da3b7b3612dc2838f3d604a
Gert Vercleyen <gert.vercleyen@protonmail.com>
2024-04-09 14:05:28 +0200

    BugFix, +ReduceSemiLinModZ

--- ReductionTools/SolveLinearSystemModZ.m
+++ ReductionTools/SolveLinearSystemModZ.m
@@ -461,7 +461,7 @@ BinEqnToRow[ eqn_, vars_, x_, simplify_ ] :=
 
 PackageScope["ConstMonSplit"]
 
-ConstMonSplit[ x_NumericQ, _ ] :=
+ConstMonSplit[ x_?NumericQ, _ ] :=
   { x, 1 };
 
 ConstMonSplit[ mon_, x_ ] :=
@@ -671,4 +671,36 @@ FactorListToSparseInput[ list_, i_, s_ ] :=
   
  *)
 
+PackageScope["ReduceSemiLinModZ"]
 
+Options[ReduceSemiLinModZ] := 
+  Options[ConsistentQ];
+
+ReduceSemiLinModZ[ mat_, rhs_, s_, opts:OptionsPattern[]  ] := 
+  Module[{ uInv, upperTriang, leftHandSides, rightHandSides },
+    
+    { uInv, upperTriang  } =
+      HermiteDecomposition[mat];
+
+    (* rank of system *)
+    r = 
+      Length[ upperTriang ] - Count[ upperTriang, { 0 .. } ];
+    
+    expRHS =
+      PowerDot[ rhs, uInv ];
+
+    (* New right hand side of system should be trivial for trivial LHS *)
+    consistent =
+      AddOptions[opts][ConsistentQ][ r, expRHS, TrueQ[# == 1]& ];
+      
+    If[ !consistent, Return @ { False } ];
+
+    rightHandSides = 
+      expRHS[[;;r]];
+
+    leftHandSides = 
+      PowerDot[ Array[ s, Last @ Dimensions[mat] ], upperTriang[[;;r]] ];
+
+    Thread[ leftHandSides == rightHandSides ]
+
+  ]
