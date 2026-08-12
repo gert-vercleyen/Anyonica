7717a8a8e2f9219f8342e0a80f3cdcb64a7c56cb
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-08-12 16:08:09 +0200

    updated pacletinfo

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,18 +1,5 @@
-PacletObject[<|
-  "Name" -> "Anyonica"
-  ,
-  "Version" -> "0.9.20.1"
-  ,
-  "Extensions" ->
-    {
-      {
-        "Kernel"
-        ,
-        "Root" -> "Kernel"
-        ,
-        "Context" -> "Anyonica`"
-      }
-      ,
-      { "Documentation", Language -> "English", "MainPage" -> "Guides/Anyonica" }
-    }
-|>]
+PacletObject[<| "Name"->"Anyonica", "Version"->"0.9.20.2",
+              "Extensions"->{
+                  {"Kernel", "Root"->"Kernel", "Context"->"Anyonica`"},
+                  {"Documentation", Language->"English",
+                   "MainPage"->"Guides/Anyonica"}} |>]
